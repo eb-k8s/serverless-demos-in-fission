d8e1bbc1da86f4a35a08c8b8647516ecf20aad07
GreyBalloonYU <yuxinzhou@ebupt.com>
2022-06-10 10:32:04 +0800

    add opentelemetry support for adservice

--- gcp-microservices-demo/src/adservice/server.go
+++ gcp-microservices-demo/src/adservice/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"math/rand"
 	"net/http"
@@ -10,6 +11,16 @@ import (
 
 	"github.com/eb-k8s/serverless-demos-in-fission/gcp-microservices-demo/src/adservice/rest"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const MAX_ADS_TO_SERVE int = 2
@@ -18,6 +29,59 @@ var (
 	adservice *Adservice
 	log       *logrus.Logger
 )
+
+// Initializes an OTLP exporter, and configures the corresponding trace and
+// metric providers.
+func initProvider() {
+	ctx := context.Background()
+	// Get Resource
+	res := resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceNameKey.String("adservice"))
+
+	// Get Exporter
+	traceExporter, err := getTraceExporter(ctx)
+	if err != nil {
+		log.Fatalf("%s: %v", "failed to create trace exporter", err)
+	}
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithResource(res),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+	)
+	if traceExporter != nil {
+		bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+		tracerProvider.RegisterSpanProcessor(bsp)
+	}
+	otel.SetTracerProvider(tracerProvider)
+
+	// set global propagator to tracecontext (the default is no-op).
+	propagators := []propagation.TextMapPropagator{
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	}
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagators...))
+}
+
+func getTraceExporter(ctx context.Context) (*otlptrace.Exporter, error) {
+	otel_collector_addr := os.Getenv("OTEL_COLLECTOR_ADDR")
+	if otel_collector_addr == "" {
+		log.Info("OTEL_COLLECTOR_ADDR not set, skipping Opentelemtry tracing")
+		return nil, nil
+	}
+	log.Infof("adservice with opentelemetry collector: %s\n", otel_collector_addr)
+	grpcOpts := []otlptracegrpc.Option{
+		otlptracegrpc.WithEndpoint(otel_collector_addr),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()),
+		otlptracegrpc.WithInsecure(),
+	}
+	// Set up a trace exporter
+	traceExporter, err := otlptracegrpc.New(ctx, grpcOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return traceExporter, nil
+}
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
@@ -36,27 +100,50 @@ func init() {
 	adservice = new(Adservice)
 	adservice.Set_max_ads_to_serve(MAX_ADS_TO_SERVE)
 	adservice.CreateAdsMap()
+
+	initProvider()
 }
 
 // Handler is the entry point for this fission function
 func Handler(w http.ResponseWriter, r *http.Request) {
+	var tracer trace.Tracer
+	if span := trace.SpanFromContext(r.Context()); span.SpanContext().IsValid() {
+		tracer = span.TracerProvider().Tracer("")
+	} else {
+		tracer = otel.GetTracerProvider().Tracer("")
+	}
+	// Extract context from carrier
+	propagators := otel.GetTextMapPropagator()
+	ctx := propagators.Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+	// Start a span
+	_, span := tracer.Start(
+		ctx,
+		"handle request",
+		trace.WithSpanKind(trace.SpanKindServer),
+	)
+	defer span.End()
+
 	if r.Method != "GET" {
 		log.Errorf("methods other than GET are not supported")
 		w.WriteHeader(http.StatusBadRequest)
+		span.AddEvent("methods other than GET are not supported")
 		return
 	}
+	span.AddEvent("invoke GetAds")
 	v := r.URL.Query()
 	if v.Has("context_keys") {
 		result, err := adservice.GetAds(&rest.AdRequest{ContextKeys: strings.Split(v.Get("context_keys"), ",")})
 		if err != nil {
 			log.Error(err)
 			w.WriteHeader(http.StatusBadRequest)
+			span.AddEvent("an error occurred in GetAds")
 			return
 		}
 		body, err := json.Marshal(result)
 		if err != nil {
 			log.Error(err)
 			w.WriteHeader(http.StatusBadRequest)
+			span.AddEvent("an error occurred in GetAds")
 			return
 		}
 		w.Header().Set("content-type", "application/json")
@@ -64,13 +151,16 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Error(err)
 			w.WriteHeader(http.StatusBadRequest)
+			span.AddEvent("an error occurred in GetAds")
 			return
 		}
 	} else {
 		log.Errorf("cannot get context_keys")
 		w.WriteHeader(http.StatusBadRequest)
+		span.AddEvent("cannot get context_keys")
 		return
 	}
+	span.AddEvent("successfully handle request")
 }
 
 type Adservice struct {
