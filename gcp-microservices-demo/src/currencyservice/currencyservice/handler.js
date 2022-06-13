d5630d68178b0d3fa570a59e40e18f95e6a0b6c7
GreyBalloonYU <yuxinzhou@ebupt.com>
2022-06-13 22:40:43 +0800

    add opentelemetry support for currencyservice

--- gcp-microservices-demo/src/currencyservice/currencyservice/handler.js
+++ gcp-microservices-demo/src/currencyservice/currencyservice/handler.js
@@ -8,18 +8,36 @@ const logger = pino({
     useLevelLabels: true
 });
 
+const api = require('@opentelemetry/api')
+const { defaultTextMapGetter, ROOT_CONTEXT, SpanKind } = require('@opentelemetry/api');
+const tracer = require('./tracer')('currencyservice');
+
 var currencyservice = new rest.CurrencyService();
 
 module.exports = async function(context) {
+    const parentCtx = api.propagation.extract(ROOT_CONTEXT, context.request.headers, defaultTextMapGetter)
+    const span = tracer.startSpan(
+        'handle request',
+        {
+            kind: SpanKind.SERVER,
+        },
+        parentCtx,
+    )
+
     if (context.request.method == "GET") {  //GetSupportedCurrencies
         try {
+            span.addEvent("invoke GetSupportedCurrencies");
             var resp = currencyservice.getSupportedCurrencies();
+            span.addEvent("successfully handle request");
+            span.end();
             return {
                 status: 200,
                 body: resp
             }
         } catch(err) {
             logger.error(err);
+            span.addEvent("an error occurred in GetSupportedCurrencies");
+            span.end();
             return {
                 status: 400,
                 body: new rest.GetSupportedCurrenciesResponse()
@@ -27,6 +45,7 @@ module.exports = async function(context) {
         }
     } else if (context.request.method == "POST") {    //Convert
         try {
+            span.addEvent("invoke Convert");
             var req = new rest.CurrencyConversionRequest(
                 new rest.Money(
                     context.request.body.from.currency_code,
@@ -36,12 +55,16 @@ module.exports = async function(context) {
                 context.request.body.to_code
             );
             var resp = currencyservice.convert(req);
+            span.addEvent("successfully handle request");
+            span.end();
             return {
                 status: 200,
                 body: resp
             }
         } catch(err) {
             logger.error(err);
+            span.addEvent("an error occurred in Convert");
+            span.end();
             return {
                 status: 400,
                 body: new rest.Money()
@@ -49,6 +72,8 @@ module.exports = async function(context) {
         }
     } else {
         logger.error("methods other than GET and POST are not supported");
+        span.addEvent("methods other than GET and POST are not supported");
+        span.end();
         return {
             status: 400
         }
---
+++ gcp-microservices-demo/src/currencyservice/currencyservice/tracer.js
@@ -0,0 +1,40 @@
+const api = require('@opentelemetry/api');
+const grpc = require('@grpc/grpc-js');
+const { BasicTracerProvider, BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { W3CTraceContextPropagator } = require('@opentelemetry/core');
+const pino = require('pino');
+
+const logger = pino({
+    name: 'currencyservice',
+    messageKey: 'message',
+    levelKey: 'severity',
+    useLevelLabels: true
+});
+
+var endpoint = process.env.OTEL_COLLECTOR_ADDR
+
+module.exports = (serviceName) => {
+    const provider = new BasicTracerProvider({
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+        })
+    });
+    if (endpoint == "") {
+        logger.info("OTEL_COLLECTOR_ADDR not set, skipping Opentelemtry tracing")
+        provider.register();
+    } else {
+        logger.info("cartservice with opentelemetry collector: %s", endpoint);
+        const collectorOptions = {
+            url: endpoint,
+            credentials: grpc.credentials.createInsecure(),
+        };
+        const exporter = new OTLPTraceExporter(collectorOptions);
+        provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+        provider.register();
+    }
+    api.propagation.setGlobalPropagator(new W3CTraceContextPropagator())
+    return provider.getTracer('');
+}
